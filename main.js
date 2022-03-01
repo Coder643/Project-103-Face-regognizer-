83bcfff6ff0d7cff3ba1fb3d5422719932ecc1dc
Coder643 <79839709+Coder643@users.noreply.github.com>
2022-03-01 19:55:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,6 +20,23 @@ console.log("ml5 version"+ml5.version);
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/jHVTOZfjR/",modelLoaded);
 
 function modelLoaded(){
-    
+    console.log("model Loaded");
+
 }
     
+function check() {
+    img = document.getElementById("selfie_image");
+
+    classifier.classify(img, goResult);
+    
+}
+
+function goResult(error , results){
+    if (error) {
+        console.log(error);
+    }else{
+        console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
